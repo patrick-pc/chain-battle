d195f3d8c8009d666eb77f0c2900cc804e3688f5
Peter Parker <web3slinger@gmail.com>
2022-05-17 13:25:16 -0500

    Update contract

--- contracts/ChainBattles.sol
+++ contracts/ChainBattles.sol
@@ -10,23 +10,55 @@ import "@openzeppelin/contracts/utils/Base64.sol";
 contract ChainBattles is ERC721URIStorage {
     using Strings for uint256;
     using Counters for Counters.Counter;
-    Counters.Counter private _tokenIds;
+    Counters.Counter private _tokenId;
 
-    mapping(uint256 => uint256) public tokenIdToLevels;
+    struct CharacterAttributes {
+        string name;
+        string class;
+        uint256 strength;
+        uint256 dexterity;
+        uint256 intelligence;
+        uint256 level;
+    }
+
+    CharacterAttributes[] characters;
+
+    mapping(uint256 => CharacterAttributes) public nftHolderAttributes;
 
     constructor() ERC721("Chain Battles", "CBTLS") {}
 
-    function generateCharacter(uint256 tokenId) public returns (string memory) {
+    function generateCharacter(uint256 tokenId)
+        public
+        view
+        returns (string memory)
+    {
         bytes memory svg = abi.encodePacked(
             '<svg xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMinYMin meet" viewBox="0 0 350 350">',
             "<style>.base { fill: white; font-family: serif; font-size: 14px; }</style>",
             '<rect width="100%" height="100%" fill="black" />',
+            '<text x="50%" y="30%" class="base" dominant-baseline="middle" text-anchor="middle">',
+            "Name: ",
+            getName(tokenId),
+            "</text>",
             '<text x="50%" y="40%" class="base" dominant-baseline="middle" text-anchor="middle">',
-            "Warrior",
+            "Class: ",
+            getClass(tokenId),
             "</text>",
             '<text x="50%" y="50%" class="base" dominant-baseline="middle" text-anchor="middle">',
-            "Levels: ",
-            getLevels(tokenId),
+            "STR: ",
+            getSTR(tokenId),
+            "</text>",
+            '<text x="50%" y="60%" class="base" dominant-baseline="middle" text-anchor="middle">',
+            "DEX: ",
+            getDEX(tokenId),
+            "</text>",
+            '<text x="50%" y="70%" class="base" dominant-baseline="middle" text-anchor="middle">',
+            "INT: ",
+            getINT(tokenId),
+            "</text>",
+            '<text x="50%" y="80%" class="base" dominant-baseline="middle" text-anchor="middle">',
+            "Level: ",
+            getLevel(tokenId),
             "</text>",
             "</svg>"
         );
@@ -39,12 +71,35 @@ contract ChainBattles is ERC721URIStorage {
             );
     }
 
-    function getLevels(uint256 tokenId) public view returns (string memory) {
-        uint256 levels = tokenIdToLevels[tokenId];
-        return levels.toString();
+    function getName(uint256 tokenId) public view returns (string memory) {
+        return nftHolderAttributes[tokenId].name;
     }
 
-    function getTokenURI(uint256 tokenId) public returns (string memory) {
+    function getClass(uint256 tokenId) public view returns (string memory) {
+        return nftHolderAttributes[tokenId].class;
+    }
+
+    function getSTR(uint256 tokenId) public view returns (string memory) {
+        uint256 str = nftHolderAttributes[tokenId].strength;
+        return str.toString();
+    }
+
+    function getDEX(uint256 tokenId) public view returns (string memory) {
+        uint256 dex = nftHolderAttributes[tokenId].dexterity;
+        return dex.toString();
+    }
+
+    function getINT(uint256 tokenId) public view returns (string memory) {
+        uint256 intelligence = nftHolderAttributes[tokenId].intelligence;
+        return intelligence.toString();
+    }
+
+    function getLevel(uint256 tokenId) public view returns (string memory) {
+        uint256 level = nftHolderAttributes[tokenId].level;
+        return level.toString();
+    }
+
+    function getTokenURI(uint256 tokenId) public view returns (string memory) {
         bytes memory dataURI = abi.encodePacked(
             "{",
             '"name": "Chain Battles #',
@@ -66,10 +121,17 @@ contract ChainBattles is ERC721URIStorage {
     }
 
     function mint() public {
-        _tokenIds.increment();
-        uint256 newItemId = _tokenIds.current();
+        _tokenId.increment();
+        uint256 newItemId = _tokenId.current();
         _safeMint(msg.sender, newItemId);
-        tokenIdToLevels[newItemId] = 0;
+        nftHolderAttributes[newItemId] = CharacterAttributes({
+            name: "Spidey",
+            class: "Assassin",
+            strength: 10 + random(40),
+            dexterity: 10 + random(30),
+            intelligence: 10 + random(20),
+            level: 1
+        });
         _setTokenURI(newItemId, getTokenURI(newItemId));
     }
 
@@ -79,8 +141,27 @@ contract ChainBattles is ERC721URIStorage {
             ownerOf(tokenId) == msg.sender,
             "Yout must own this NFT to train it!"
         );
-        uint256 currentLevel = tokenIdToLevels[tokenId];
-        tokenIdToLevels[tokenId] = currentLevel + 1;
+        uint256 currentSTR = nftHolderAttributes[tokenId].strength;
+        uint256 currentDEX = nftHolderAttributes[tokenId].dexterity;
+        uint256 currentINT = nftHolderAttributes[tokenId].intelligence;
+        uint256 currentLevel = nftHolderAttributes[tokenId].level;
+        nftHolderAttributes[tokenId].strength = currentSTR + random(3);
+        nftHolderAttributes[tokenId].dexterity = currentDEX + random(2);
+        nftHolderAttributes[tokenId].intelligence = currentINT + random(1);
+        nftHolderAttributes[tokenId].level = currentLevel + 1;
         _setTokenURI(tokenId, getTokenURI(tokenId));
+    }
+
+    function random(uint256 number) public view returns (uint256) {
+        return
+            uint256(
+                keccak256(
+                    abi.encodePacked(
+                        block.timestamp,
+                        block.difficulty,
+                        msg.sender
+                    )
+                )
+            ) % number;
     }
 }
